bf930c53d03b50aa1428361ee6d81af59343aa34
Marcos Aurelio <marcosaurelio.rn@gmail.com>
2022-07-16 12:04:30 -0300

    Adding update action to the users controller with tests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,6 +20,16 @@ respond_to :json
     end
   end
 
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user, status: 200
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
   private
 
   def user_params
